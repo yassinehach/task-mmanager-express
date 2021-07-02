0c411dd7d3b40b63cdc5cc6429fd4bd2201f7401
Tuxette Chix <tuxette@chix.nathalievilla.org>
2021-07-02 11:22:59 +0100

    structure

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express()
+const port = 3000
+
+app.get('/hello',(req,res)=>{
+    res.send('<h1>Task manager app</h1>')
+})
+
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`)
+} )
